58b045d337e285aa8708311e3bc2e3464638bdb4
deadsplash <splashyo@deniss-MacBook-Pro.local>
2023-08-11 17:46:06 +0400

    rm unused

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    BOT_TOKEN: str = "6674201207:AAFWV4Agjr-Lai4SUYAM3_poPTMPonZefBA"
-    POSTGRES_URI: str = "postgresql://bot:osatgbot@94.103.93.61:5432/dev_db"
+    BOT_TOKEN: str = ""
+    POSTGRES_URI: str = ""
     ENV: str = "DEV"
 
 
